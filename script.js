3096ad1ff0b196135bcfac106cd34a439c08a88a
s <mwofohadriel@gamil.com>
2024-04-22 22:41:53 +0100

    added a script.js file

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector('.btn-1').addEventListener('click', function() {
+  alert('Hire Me button clicked!');
+});
+
+document.querySelector('.btn-2').addEventListener('click', function() {
+  alert('Experience button clicked!');
+});
