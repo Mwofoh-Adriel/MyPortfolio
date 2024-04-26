33e647f6dbf5da649a798f8cbe282694266033cb
s <mwofohadriel@gamil.com>
2024-04-26 17:42:05 +0100

    modified and added some files

--- script.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('.btn-1').addEventListener('click', function() {
-  alert('Hire Me button clicked!');
-});
-
-document.querySelector('.btn-2').addEventListener('click', function() {
-  alert('Experience button clicked!');
-});
